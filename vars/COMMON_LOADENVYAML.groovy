e8264f8958188ab0b7fc9237b2600f8ad07dbe26
Uegi <uegi.yu@dynasafe.com.tw>
2023-07-13 16:00:24 +0800

    v1

--- vars/COMMON_LOADENVYAML.groovy
+++ vars/COMMON_LOADENVYAML.groovy
@@ -1,5 +1,5 @@
-def variableTextPath = libraryResource "variableDefinition/variable.yaml"
-def CACHE_YAML_OBJ = readYaml text: variableTextPath
 def call(final String envName) {       
+    def variableTextPath = libraryResource "variableDefinition/variable.yaml"
+    def CACHE_YAML_OBJ = readYaml text: variableTextPath
     return CACHE_YAML_OBJ[envName]
 }
