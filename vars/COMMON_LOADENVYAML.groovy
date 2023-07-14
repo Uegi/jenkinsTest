4ac6d6fdc6bde82c0212acc836cec4cb4b2066f3
Uegi <uegi.yu@dynasafe.com.tw>
2023-07-14 14:09:58 +0800

    v1

--- vars/COMMON_LOADENVYAML.groovy
+++ vars/COMMON_LOADENVYAML.groovy
@@ -1,8 +1,8 @@
 def call(final String envName,final String configValue) {       
     def variableTextPath = libraryResource "variableDefinition/variable.yaml"
     def templateContent = readYaml text: variableTextPath
-    def modifiedYaml = templateContent.replace('${platformProjectName}', "sit")
-    writeYaml file: variableTextPath, data: modifiedYaml
-    def finishContent = readYaml text: variableTextPath
-    return finishContent[envName][configValue]
+    //def modifiedYaml = templateContent.replace('${platformProjectName}', "sit")
+    //writeYaml file: variableTextPath, data: modifiedYaml
+    //def finishContent = readYaml text: variableTextPath
+    return templateContent[envName][configValue]
 }
